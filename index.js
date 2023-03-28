c698845c92ab86b141b823a1349eca6d72a99528
Yeasin Arafat <86848650+Yea-sin@users.noreply.github.com>
2023-03-28 19:46:27 +0600

    app structure in progress,,,

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// NODE UPTIME MONITORING APP
+
+// dependencies
+const http = require('http');
 const url = require('url');
 
-console.log(url);
+// app object - module sca-folding
+const app = {};
+
+// config
+app.config = {
+    port:3000
+};
+
+// create server
+app.createServer  = () =>{
+    const server = http.createServer(app.handelReqRes);
+    server.listen(app.config.port, ()=>{
+        console.log(`listening to port ${app.config.port}`);
+    })
+
+};
+
+// handel request response
+app.handelReqRes = (req, res) => {
+    // handel req url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.path.replace(/^\/|\/$/g, '');
+    const queryParamsObject = parsedUrl.query;
+    const headersObject = req.headers;
+    console.log(headersObject);
+
+     res.end('Server Started')
+}
+
+// start server
+app.createServer();
